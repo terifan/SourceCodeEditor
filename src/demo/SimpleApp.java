a6753fd724216e06e6dbbf5f5694cb9d9033d6fb
terifan <pinjata69@gmail.com>
2022-01-18 14:51:57 +0100

    update

--- src/demo/SimpleApp.java
+++ src/demo/SimpleApp.java
@@ -14,27 +14,27 @@ public class SimpleApp
 	{
 		try
 		{
-			SyntaxParser parser = new JavaSyntaxParser();
-			Document document = new Document(
-				"public static void main(String[] args)\n" +
-				"{\n" +
-				"	try\n" +
-				"	{\n" +
-				"		System.out.println(\"3 x 5 = \" + 3 * 5);\n" +
-				"	}\n" +
-				"	catch (Exception e)\n" +
-				"	{\n" +
-				"		e.printStackTrace(System.out);\n" +
-				"	}\n" +
-				"}"
-			);
+SyntaxParser parser = new JavaSyntaxParser();
+Document document = new Document(
+	"public static void main(String[] args)\n" +
+	"{\n" +
+	"	try\n" +
+	"	{\n" +
+	"		System.out.println(\"3 x 5 = \" + 3 * 5);\n" +
+	"	}\n" +
+	"	catch (Exception e)\n" +
+	"	{\n" +
+	"		e.printStackTrace(System.out);\n" +
+	"	}\n" +
+	"}"
+);
 
-			JFrame frame = new JFrame();
-			frame.add(new JScrollPane(new SourceEditor(parser, document)));
-			frame.setSize(1024, 768);
-			frame.setLocationRelativeTo(null);
-			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			frame.setVisible(true);
+JFrame frame = new JFrame();
+frame.add(new JScrollPane(new SourceEditor(parser, document)));
+frame.setSize(1024, 768);
+frame.setLocationRelativeTo(null);
+frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+frame.setVisible(true);
 		}
 		catch (Throwable e)
 		{
